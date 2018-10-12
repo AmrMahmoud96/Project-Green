fb3b0ce94cbaa291774d03cc0efc6d603f951a47
amrmahmoud <amrmahmoud.am@hotmail.com>
2018-10-12 18:27:33 -0400

    init test project

---
+++ initial project/test.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/simple_chart")
+def template_test():
+    return render_template('base.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+ 
+@app.route("/")
+def chart():
+    legend = 'Monthly Data'
+    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
+    values = [10, 9, 8, 7, 6, 4, 7, 8]
+    return render_template('chart.html', values=values, labels=labels, legend=legend)
+ 
+
+if __name__ == '__main__':
+    app.run(debug=True)
