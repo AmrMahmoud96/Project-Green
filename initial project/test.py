0e0df82ee10945c3f9ae9802a4145e3e08e8ec3a
amrmahmoud <amrmahmoud.am@hotmail.com>
2018-10-13 01:28:27 -0400

    init pages

--- initial project/test.py
+++ initial project/test.py
@@ -5,7 +5,22 @@ app = Flask(__name__)
 @app.route("/simple_chart")
 def template_test():
     return render_template('base.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
- 
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+@app.route("/contactus")
+def contactus():
+    return render_template("contactus.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/joinus")
+def joinus():
+    return render_template("joinus.html")
+
 @app.route("/")
 def chart():
     legend = 'Monthly Data'
