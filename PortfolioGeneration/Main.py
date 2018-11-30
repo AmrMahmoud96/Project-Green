721272b132fd20f75c26360c3651c8be3edbb80b
Ameer <ameershaikh02@gmail.com>
2018-11-30 12:51:36 -0500

    Updates

--- PortfolioGeneration/Main.py
+++ PortfolioGeneration/Main.py
@@ -613,7 +613,7 @@ if __name__ == "__main__":
     #plt.rcParams['axes.prop_cycle'] = plt.cycler(color=['#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#42d4f4', '#f032e6', '#bfef45', '#fabebe', '#469990', '#e6beff', '#9A6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#a9a9a9'])
     #set Leverage (1 means no leverage)
     ###############
-    leverage = 3.5
+    leverage = 1.0
     ###############
     
     #load prices
