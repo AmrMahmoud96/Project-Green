281b868878c23df79ba185c5c8d4a0fdacd92771
Ameer <ameershaikh02@gmail.com>
2018-11-30 22:12:31 -0500

    Changes function_1_2

--- PortfolioGeneration/function_1_2.py
+++ PortfolioGeneration/function_1_2.py
@@ -22,15 +22,12 @@ class portfolio_one:
         Dollars = pd.Series(values,index=etfs)
         self.initial_value = Dollars.sum()
         Weights = Dollars/Dollars.sum()
-        #Prices = pd.DataFrame()
-       
-        #get price data for relevant ETFs from mongoDB
-        #for etf in etfs:
-        #    Prices = fetch_data(Prices,etf)        
-        
-        #clean and calculate returns
-        #Prices.dropna(inplace=True,how='any')
-        #Returns = Prices.pct_change()
+    
+        #set market factor
+        self.Mkt = Returns['SPY']
+                
+        #load risk free rate
+        self.Rf = load_risk_free()        
         
         #return series of the portfolio
         self.returns = (Returns[etfs]*Weights).sum(axis=1) 
@@ -40,9 +37,7 @@ class portfolio_one_b:
     def __init__(self,portfolio):
         '''portfolio should indicate which of alpha factory portfolios, initial value is dollar value'''
         
-        #get retrun series from mongoDB
-        #currently only works for one portfolio
-
+        #get return series from mongoDB
         rets = pd.DataFrame(list(db['Portfolio_Returns'].find({},{"Date":1,portfolio:1,'_id': 0})))
         
         #set index to date
@@ -54,6 +49,12 @@ class portfolio_one_b:
         rets.index = pd.to_datetime(rets.index)            
         rets.sort_index(inplace=True)
         
+        #set market factor
+        self.Mkt = load_market_factor()     
+        
+        #load risk free rate
+        self.Rf = load_risk_free()          
+        
         #return series of the portfolio
         self.returns = rets[portfolio]         
 
@@ -62,6 +63,12 @@ class portfolio_mvo:
     def __init__(self,etfs,weights,returns):
         
         Weights = pd.Series(weights,index=etfs)
+        
+        #load risk free rate
+        self.Rf = load_risk_free()  
+        
+        #set market factor
+        self.Mkt = returns['SPY']
         
         #returns series of the portfolio
         self.returns = (returns*Weights).sum(axis=1)
@@ -89,25 +96,25 @@ def portfolio_value_ts(returns,initial_value,startdate=None,enddate=None):
     
     return cumul_value
     
-def portfolio_stats(returns,Rf,Mkt,startdate=None,enddate=None):
+def portfolio_stats(portfolio,startdate=None,enddate=None):
     '''calculate statistics of the portfolio over the given date range'''
     
     #if no startdate and endate will default to all data
     if not startdate and not enddate:
-        rets = returns
+        rets = portfolio.returns
     elif not startdate and enddate:
-        rets = returns[(returns.index >= startdate)]
+        rets = portfolio.returns[(portfolio.returns.index >= startdate)]
     elif startdate and not enddate:
-        rets = returns[(returns.index <= enddate)]
+        rets = portfolio.returns[(portfolio.returns.index <= enddate)]
     else:
         #trim data based on dates
-        rets = returns[(returns.index >= startdate) & (returns.index <= enddate)]        
+        rets = portfolio.returns[(portfolio.returns.index >= startdate) & (portfolio.returns.index <= enddate)]        
 
     #assume no return on first day, time series represents EOD value
     rets = rets.iloc[1::]    
     
     #calculation of excess return of 3-month US T-bill
-    excess_rets = (rets - Rf['Rate']).dropna()
+    excess_rets = (rets - portfolio.Rf['Rate']).dropna()
     
     #calculate cumulative returns (used for calculating some statistics)
     cumul_rets = (1 + rets).cumprod()        
@@ -141,7 +148,7 @@ def portfolio_stats(returns,Rf,Mkt,startdate=None,enddate=None):
     CVaR = Hist_CVaR(1,VaR,rets)    
     
     #9. Beta, Alpha, R-Squared
-    beta, alpha, R2 = beta_calc(rets, Mkt)
+    beta, alpha, R2 = beta_calc(rets, portfolio.Mkt)
     
     #annualize alpha
     alpha = ((1+alpha)**252)-1
@@ -221,6 +228,13 @@ def load_risk_free():
     #convert the index as date
     temp_df.index = pd.to_datetime(temp_df.index)  
     return temp_df
+
+def load_market_factor():
+    temp_df = pd.DataFrame(list(db['Raw_Prices'].find({},{'Date':1,'SPY': 1,'_id': 0})))
+    temp_df.set_index('Date',inplace=True)
+    #convert the index as date
+    temp_df.index = pd.to_datetime(temp_df.index)  
+    return temp_df['SPY'].replace("",np.nan).pct_change()
     
 def compare_portfolios(startdate,enddate,etfs,values):
     ''''''
@@ -242,12 +256,6 @@ def compare_portfolios(startdate,enddate,etfs,values):
     #clean and calculate returns
     Prices.dropna(inplace=True,how='any')
     returns = Prices[etf_uni].pct_change()   
-        
-    #load risk free rate
-    Rf = load_risk_free()
-    
-    #set market factor
-    Mkt = Prices['SPY'].pct_change()
     
     #if no startdate and endate will default to all data
     if not startdate and not enddate:
@@ -270,7 +278,7 @@ def compare_portfolios(startdate,enddate,etfs,values):
     user_port_ts = portfolio_value_ts(user_port.returns,user_port.initial_value,None, None)
     
     #calculate stats for user portfolio
-    user_port_stats = portfolio_stats(user_port.returns,Rf,Mkt,None, None)
+    user_port_stats = portfolio_stats(user_port,None, None)
     
     #convert return back to average
     return_target = ((user_port_stats['CAGR']+1)**(1/252))-1        
@@ -279,7 +287,7 @@ def compare_portfolios(startdate,enddate,etfs,values):
     port_type = 'Balanced'
     our_port = portfolio_one_b(port_type)
     #calculate stats for all portfolios
-    our_port_stats = portfolio_stats(our_port.returns,Rf,Mkt,startdate,enddate)
+    our_port_stats = portfolio_stats(our_port,startdate,enddate)
     if our_port_stats['Sharpe'] >= user_port_stats['Sharpe']:
         dom_port = our_port
         dom_port_stats = our_port_stats
@@ -299,7 +307,7 @@ def compare_portfolios(startdate,enddate,etfs,values):
         
         #calculate characteristics of mvo portfolio
         dom_port = portfolio_mvo(etf_uni,MVO_wgts,rets)
-        dom_port_stats = portfolio_stats(dom_port.returns,Rf,Mkt,None,None)
+        dom_port_stats = portfolio_stats(dom_port,None,None)
         dom_port_ts = portfolio_value_ts(dom_port.returns,user_port.initial_value,None,None)        
         
     return user_port, user_port_ts, user_port_stats, dom_port, dom_port_ts, dom_port_stats
@@ -342,42 +350,38 @@ if __name__ == "__main__":
     
     #test 1(Their Portfolio):
     #initialize portfolio
-    #Rf = load_risk_free()
     
     ###get price data for all ETFs from mongoDB
-    #Prices = fetch_all_data()
+    Prices = fetch_all_data()
     
     ###clean and calculate returns
-    #etf_uni = ['ACWV','AGG','DBC','EMB','EMGF','GLD','HYG','IMTM','IQLT','IVLU','MTUM','QUAL','SCHH','SIZE','SPTL','TIP','USMV','VLUE','SHV','SPY']
-    #Prices.dropna(inplace=True,how='any')
-    #returns = Prices[etf_uni].pct_change()   
+    etf_uni = ['ACWV','AGG','DBC','EMB','EMGF','GLD','HYG','IMTM','IQLT','IVLU','MTUM','QUAL','SCHH','SIZE','SPTL','TIP','USMV','VLUE','SHV','SPY']
+    Prices.dropna(inplace=True,how='any')
+    returns = Prices[etf_uni].pct_change()   
+
+    #test_portfolio = portfolio_one(['SPY','AGG','SCHH','DBC'],[100,100,100,100],returns)
     
-    #Mkt = Prices['SPY'].pct_change()
+    ####get time series of portfolio value
+    #ts_value_1 = portfolio_value_ts(test_portfolio.returns,test_portfolio.initial_value,None,None)
     
-    ##test_portfolio = portfolio_one(['SPY','AGG','SCHH','DBC'],[100,100,100,100],returns)
+    ####plot data
+    #ts_value_1.plot()
+    #plt.show()
     
-    ###get time series of portfolio value
-    ##ts_value_1 = portfolio_value_ts(test_portfolio.returns,test_portfolio.initial_value,None,None)
+    ##get time series for specific date range
+    #ts_value_2 = portfolio_value_ts(test_portfolio.returns,test_portfolio.initial_value,datetime(2011,1,1),datetime(2013,1,1))
     
-    ###plot data
-    ##ts_value_1.plot()
-    ##plt.show()
+    ##plot data
+    #ts_value_2.plot()
+    #plt.show()
     
-    ###get time series for specific date range
-    ##ts_value_2 = portfolio_value_ts(test_portfolio.returns,test_portfolio.initial_value,datetime(2011,1,1),datetime(2013,1,1))
+    ####get portfolio stats using all data
+    #port_stats_1 = portfolio_stats(test_portfolio,None,None)
     
-    ###plot data
-    ##ts_value_2.plot()
-    ##plt.show()
+    ##get portfolio stats for a specific date range
+    #port_stats_2 = portfolio_stats(test_portfolio,datetime(2011,1,1),datetime(2013,1,1))
     
-    ###get portfolio stats using all data
-    ##port_stats_1 = portfolio_stats(test_portfolio.returns,Rf,Mkt,None,None)
-    
-    ###get portfolio stats for a specific date range
-    ##port_stats_2 = portfolio_stats(test_portfolio.returns,Rf,Mkt,datetime(2011,1,1),datetime(2013,1,1))
-    
-    
-    ###test 2 (Our Portfolio):
+    ##test 2 (Our Portfolio):
     ###initialize portfolio
     #test_portfolio = portfolio_one_b('Balanced')
     
@@ -392,14 +396,14 @@ if __name__ == "__main__":
     #ts_value_2 = portfolio_value_ts(test_portfolio.returns,100,datetime(2011,1,1),datetime(2013,1,1))
     
     ###plot data
-    ##ts_value_2.plot()
-    ##plt.show()
+    #ts_value_2.plot()
+    #plt.show()
     
     ##get portfolio stats using all data
-    #port_stats_1 = portfolio_stats(test_portfolio.returns,Rf,Mkt,None,None)
+    #port_stats_1 = portfolio_stats(test_portfolio,None,None)
     
     ##get portfolio stats for a specific date range
-    #port_stats_2 = portfolio_stats(test_portfolio.returns,Rf,Mkt,datetime(2011,1,1),datetime(2013,1,1))
+    #port_stats_2 = portfolio_stats(test_portfolio,datetime(2011,1,1),datetime(2013,1,1))
     
     ###############################################
     #Main test (function 1 and function 2 combined)
@@ -412,7 +416,7 @@ if __name__ == "__main__":
     #test1 = compare_portfolios(datetime(2010,1,1),datetime(2013,1,1),['SPY','AGG','SCHH','DBC'],[100,100,100,100])
     
     #test2 should select the MVO portfolio (based on below inputs)
-    #test2 = compare_portfolios(datetime(2015,1,1),datetime(2017,1,1),['SPY','AGG','SCHH','DBC'],[100,100,100,100])
+    #test2 = compare_portfolios(datetime(2013,1,1),datetime(2016,1,1),['SPY','AGG','SCHH','DBC'],[100,100,100,100])
     
     end = time.time()
     print(end - start)    
